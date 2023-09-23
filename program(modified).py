0bb1a6da2a43c35e07b1899930614b2b2a38c57f
Victor Lu <106136532+donkey0319@users.noreply.github.com>
2023-09-22 20:43:22 -0500

    hihihi

--- program(modified).py
+++ program(modified).py
@@ -4,52 +4,33 @@ import time
 
 import csv
 
-import datetime
+from datetime import datetime
+
+from datetime import date
 
 
-
+# GPIO on Pi
 DT = 5
-
 SCK= 6
 
-
-HIGH=1
-
-LOW=0
-
-
-sample=0
-
-val=0
-
-
 gpio.setwarnings(False)
-
 gpio.setmode(gpio.BCM)
-
-
 gpio.setup(SCK, gpio.OUT)
 
-
+# output of count without connecting the load cell, can use it to tare
 
 def readCount():
 
   i=0
-
   Count=0
 
  # print Count
-
  # time.sleep(0.001)
 
   gpio.setup(DT, gpio.OUT)
-
   gpio.output(DT,1)
-
   gpio.output(SCK,0)
-
   gpio.setup(DT, gpio.IN)
-
 
   while gpio.input(DT) == 1:
 
@@ -57,7 +38,8 @@ def readCount():
 
   for i in range(24):
 
-        gpio.output(SCK,1)
+        gpio.output(SCK,1)        
+
 
         Count = Count<<1
 
@@ -76,33 +58,57 @@ def readCount():
 
   gpio.output(SCK,1)
 
-  Count=Count^0x800000
+  Count=Count^0x800000 # xor = ^
 
   #time.sleep(0.001)
 
   gpio.output(SCK,0)
 
-  return Count  
+  return Count
 
-with open('raw_data.csv', 'wb') as csvfile:
+
+
+#### Write to csv ####\
+
+
+print("Please remove anything on the weight scale: (press enter to continue)")
+c = input("")
+x1 = readCount()
+y1 = 0
+print("Please put some weight on the weight scale (kg):")
+y2 = float(input(""))
+x2 = readCount()
+
+a = (y2-y1)/(x2-x1)
+b = y1 - a*x1
+
+
+with open('raw_data.csv', 'w') as csvfile:
     
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow(['DateTime', 'Weight'])
+    filewriter.writerow(['Date', 'Time', 'Weight'])
     
     while 1:
 
-        current_utc = datetime.utcnow()
+        curr_date = date.today()
+        
+        time_now = datetime.now()
+        
+        curr_time = time_now.strftime("%H:%M:%S")
         
         count = readCount()
 
-        w = 0
-
-        w = (count-sample)/106        # Accurate weight proportion
-
-        print(w,"g")
+        w = count * a + b
         
-        filewriter.writerow([current_utc, w])
+        result = round(w, 3)
+
+        print("Weight: ", result, "kg")
+        
+        filewriter.writerow([curr_date, curr_time, result])
+        
+        csvfile.flush()
 
         time.sleep(0.5)
+        
 
