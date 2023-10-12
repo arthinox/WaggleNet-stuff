7d10cd9e8214a4e205ed01d291be1e1b23da65e0
Victor Lu <106136532+donkey0319@users.noreply.github.com>
2023-10-12 17:48:37 -0500

    all fixed
    add switch control to micropython

--- Weight Scale Pico Stuff/codes/weight_scale.py
+++ Weight Scale Pico Stuff/codes/weight_scale.py
@@ -2,6 +2,8 @@ from machine import Pin
 import utime
 
 SCK = Pin(6, Pin.OUT)
+switch = Pin(18, Pin.IN)
+LED = Pin(13, Pin.OUT)
 
 def readCount():
     
@@ -29,22 +31,41 @@ def readCount():
     SCK.low()
     return Count
 
-a = 4.5573*10**(-5)
-b = -376.44
+'''
+print("Please remove anything on the weight scale: (press enter to continue)")
+c = input("")
+x1 = readCount()
+y1 = 0
+print("Please put some weight on the weight scale (kg):")
+y2 = float(input(""))
+x2 = readCount()
+
+a = (y2-y1)/(x2-x1)
+b = y1 - a*x1
+'''
+
+a = -4.482101*10**(-5)
+b = 381.3089
+
+# a = 4.5573*10**(-5)
+# b = -376.44
+
+rtc = machine.RTC()
 
 with open("test.txt", "w") as data_file:
     data_file.write("Date, Time, Weight\n")
 
     while (1):
+        timestamp = rtc.datetime()
         fake_time = 0
         weight = readCount()
         weight = a * weight + b
         result = round(weight, 3)
-        text = "1,"+str(fake_time)+", "+str(weight)+"\n"
-        print("weight:", weight)
-        fake_time += 1
-        data_file.write(text)
+        print("weight:", weight)        
+        timestring="%04d-%02d-%02d %02d:%02d:%02d"%(timestamp[0:3] + timestamp[4:7])
+        data_file.write(timestring + "," + str(weight) + "\n")        
         data_file.flush()
         utime.sleep(1)
-    
+
+
 
