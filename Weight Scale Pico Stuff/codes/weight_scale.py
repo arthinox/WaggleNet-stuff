6daa6cc786d0e3d4b79f97be2b667e9b0a9ea8dc
Victor Lu <106136532+donkey0319@users.noreply.github.com>
2023-10-15 17:04:29 -0500

    add switch control to micropython program

--- Weight Scale Pico Stuff/codes/weight_scale.py
+++ Weight Scale Pico Stuff/codes/weight_scale.py
@@ -46,12 +46,62 @@ b = y1 - a*x1
 
 a = -4.482101*10**(-5)
 b = 381.3089
-
+state = 1
 # a = 4.5573*10**(-5)
 # b = -376.44
 
 rtc = machine.RTC()
+print("Flip switch to start.")
 
+while (state == 1):
+    if switch.value() == True:
+        print("Remove any objects on the scale.")
+        while (switch.value() == True):
+            LED.high()
+            utime.sleep(0.15)
+            LED.low()
+            utime.sleep(0.15)
+        state = 0
+        break
+    utime.sleep(0.15)
+    
+x1 = readCount()
+y1 = 0
+
+while (state == 0):
+    LED.low()
+    if switch.value() == True:
+        # Read zero weight data here
+        print("Place weight on scale.")
+        while (switch.value() == True):
+            LED.high()
+            utime.sleep(0.15)
+            LED.low()
+            utime.sleep(0.15)
+        state = 1
+        break
+    utime.sleep(0.15)
+
+x2 = readCount()
+y2 = 2.582
+
+while (state == 1):
+    LED.high()
+    if switch.value() == True:
+        # Read calibration weight and calculate factor
+        print("Flip switch to continue.")
+        while (switch.value == True):
+            LED.high()
+            utime.sleep(0.15)
+            LED.low()
+            utime.sleep(0.15)
+        state = 0
+        break
+    utime.sleep(0.15)
+
+a = (y2-y1)/(x2-x1)
+b = y1 - a*x1
+LED.low()
 with open("test.txt", "w") as data_file:
     data_file.write("Date, Time, Weight\n")
 
@@ -66,6 +116,7 @@ with open("test.txt", "w") as data_file:
         data_file.write(timestring + "," + str(weight) + "\n")        
         data_file.flush()
         utime.sleep(1)
+
 
 
 
